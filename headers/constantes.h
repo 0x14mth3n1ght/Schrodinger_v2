cb33f16a41eddcdfd49d7575782a35b82ddd7549
loic <none@none.com>
2022-09-21 11:28:16 +0200

    Ajout du fichier contantes.h

---
+++ headers/constantes.h
@@ -0,0 +1,22 @@
+/**
+ * @file constantes.h
+ * @brief Ce fichier contient les constantes utiles pour le projet
+ */
+
+/**
+ * @brief Constante hbar
+ * 
+ */
+double hbar = 1.0;
+
+/**
+ * @brief Constante omega
+ * 
+ */
+double omega = 1.0;
+
+/**
+ * @brief Constante m
+ * 
+ */
+double m = 1.0;
