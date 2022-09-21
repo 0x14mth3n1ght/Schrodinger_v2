76d8f5bc7a2db9791b876cfffed09f19d952e3c7
aurelien <none@none.com>
2022-09-21 12:02:31 +0200

    phi fonctionnel

--- solution.cpp
+++ solution.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <math.h>
 #include <armadillo>
+#include "headers/constantes.h"
 
-double m = 1.00;
-double w = 1.00;
-double pi = 3.14;
-
-int Hermite(int n, int z){
+/*relation de recurrence
+@param n>=0 : nème solution phi, z : position abscisse 
+@returns H(n,z)
+*/
+double Hermite(double n, double z){
         if (n==0){
                 return 1;
         }
@@ -18,7 +19,35 @@ int Hermite(int n, int z){
                 return res;
         }
 }
+/*
+@param n>=0
+@returns fact(n)
+*/
+double fact(double n){
+        if (n<=1){
+                return 1;
+        }
+        else{
+                return n*fact(n-1);
+        }
+}
+
+/*m,omega,hbar dans headers/constantes.h*/
+/*
+@param n>=0 : nème solution phi, z : position abscisse 
+@returns solution 1D schrödinger en (n,z)
+
+*/
+double phi(double n, double z){
+        double h=Hermite(n,sqrt(z*m*omega/hbar));
+        double e=exp(-m*omega*z*z/(2*hbar));
+        double c=pow(m*omega/(M_PI*hbar),0.25)/sqrt(fact(n)*pow(2,n));
+        double phi_sol=c*e*h;
+        return phi_sol;
+}
 
 int main(){
+        std::cout << 2*1.2;
+        std::cout << pow(2.0,0.25); /*attention pow renvoie 1 si int*/
 	return 0;
 }
