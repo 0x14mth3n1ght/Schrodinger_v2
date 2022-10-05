fabaf45437536777a6fb5dcb5bdd75b7209a14f8
aurelien <none@none.com>
2022-10-05 11:12:51 +0200

    modif H_0(z)=1

--- src/hermite.cpp
+++ src/hermite.cpp
@@ -17,7 +17,7 @@ arma::mat hermiteMat(int n_max, arma::vec Z)
             //2e cas i=1; H_1(z)=2z
             //3e cas i>1; H_i(z)=2z*H_(i-1)(z) - 2(i-1)*H_(i-2)(z)
             if(i==0)
-                res.col(0)=0*Z;
+                res.col(0)=1;
             else if(i==1)
                 res.col(1)=2*Z;
             else
