7af573575783c239ed872a2b1b41853e318891f4
ngo <none@none.com>
2022-10-25 18:35:29 +0200

    Maj pres, ajout test d'énergies

--- tests/test_proprietes.h
+++ tests/test_proprietes.h
@@ -24,10 +24,29 @@ public:
         psiSolution solutions(10);
         const arma::mat mat_prod_scalaires=solutions.orthoMat();
         const arma::mat res= arma::eye(arma::size(mat_prod_scalaires));
-        //On teste si ||hermiteMat - res||_infinie <= tolerance
-        TS_TRACE("Début du test : calcul matrice hermite");
+        //On teste si ||mat_prod_scalaires - res||_infinie <= tolerance
+        TS_TRACE("Début du test : calcul matrice orthonormal");
         TS_ASSERT(arma::norm(mat_prod_scalaires - res, "inf") <= tolerance);
-        TS_TRACE("Fin du test : calcul matrice hermite");
+        TS_TRACE("Fin du test : calcul matrice orthonormal");
+    }
+
+    /**
+     * @brief Test des énergies déduits des solutions de l'équation.
+     * 
+     */
+    void testEnergies( void )
+    {
+        psiSolution solutions(10);
+        const arma::vec energies=solutions.energyMat();
+        const arma::vec res= arma::eye(size(energies));
+        
+        res(0)=hbar * omega /2;
+        for(int i=1;i<size(res);i++)
+            res(i)=res(0) + hbar * omega * i;
+        
+        TS_TRACE("Début du test : énergies");
+        TS_ASSERT(arma::norm(energies - res, "inf") <= tolerance);
+        TS_TRACE("Fin du test : énergies");
     }
 
 };
