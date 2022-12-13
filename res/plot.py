ac8ad13033edfda34dd754fd749f7be92aa3b76b
ngo <none@none.com>
2022-12-13 19:35:47 +0100

    public des résultats

--- res/plot.py
+++ res/plot.py
@@ -6,11 +6,10 @@ data = np.loadtxt('res.mat')
 
 # Convertir les données en une matrice 2D
 #matrix = np.reshape(data, (374, 374))
-
 # Afficher la matrice en tant qu'image
 #plt.imshow(matrix)
-plt.imshow(data)
 plt.title("Densité nucléaire")
-plt.xlabel("z")
-plt.ylabel("x")
+plt.xlabel("z(m)")
+plt.ylabel("r(m)")
+plt.imshow(data, extent=[-20,20,-10,10])
 plt.show()
